05dd43a0f69787977990e5aca1d5788dc82be906
DasFranck <dasfranck@gmx.com>
2021-12-08 01:09:22 +0100

    Add track_page route

--- otv/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from .frontend import frontend
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(frontend)
-    return app
--- otv/frontend.py
+++ otv/frontend.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, g
+import os
+
+from flask import Blueprint, render_template, flash, redirect, url_for, g, current_app
 
 frontend = Blueprint("frontend", __name__)
 
+@frontend.route("/track/<string:track_id>")
+def track_page(track_id: str):
+    return(render_template("track.j2", track=current_app.config["tracks"][track_id]))
+
 @frontend.route("/")
 def index_page():
-    return("")
+    return(render_template("index.j2", tracks=current_app.config["tracks"]))
