3cd0e1fe969c9862477ec2c046299f59b9b4b32e
DasFranck <dasfranck@gmx.com>
2021-12-09 02:04:02 +0100

    Add format_duration and format_datetime jinja template filters

--- otv/frontend.py
+++ otv/frontend.py
@@ -1,8 +1,19 @@
-import os
+from datetime import datetime
 
 from flask import Blueprint, render_template, flash, redirect, url_for, g, current_app
 
 frontend = Blueprint("frontend", __name__)
+
+@frontend.app_template_filter()
+def format_duration(duration: int):
+    hours = duration // 3600
+    minutes = duration % 3600 // 60
+    seconds = duration % 60
+    return f"{hours}h {minutes:02}m {seconds:02}s"
+
+@frontend.app_template_filter()
+def format_datetime(input_datetime: datetime):
+    return(input_datetime.strftime("%Y-%m-%d %H:%M:%S"))
 
 @frontend.route("/track/<string:track_id>")
 def track_page(track_id: str):
