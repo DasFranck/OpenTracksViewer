219d4c4af95958c41fb58e64e9a4f06eebc7ca37
DasFranck <dasfranck@gmx.com>
2022-02-20 21:54:44 +0100

    Add get_monthly_report_name, get_activity_list and get_month_name functions in frontend blueprint

--- otv/frontend.py
+++ otv/frontend.py
@@ -1,8 +1,33 @@
+from calendar import month_name
 from datetime import datetime
+from typing import Dict, List, Set
 
 from flask import Blueprint, render_template, flash, redirect, url_for, g, current_app
+from gpxpy.gpx import GPXTrack
 
 frontend = Blueprint("frontend", __name__)
+
+
+@frontend.app_template_global()
+def get_monthly_report_name() -> Dict[str, Set[str]]:
+    monthly_reports = {}
+
+    for track in current_app.config["tracks"].values():
+        start = track.get_time_bounds()[0]
+        if (year := start.date().year) in monthly_reports:
+            monthly_reports[year].add(start.date().month)
+        else:
+            monthly_reports[year] = {start.date().month}
+
+    return monthly_reports
+
+@frontend.app_template_global()
+def get_activity_list() -> Set[str]:
+    return {track.tracks[0].type for track in current_app.config["tracks"].values()}
+
+@frontend.app_template_filter()
+def get_month_name(month_number: int) -> str:
+    return month_name[month_number]
 
 @frontend.app_template_filter()
 def get_activity_emoji(activity_name: str, with_text: bool = False) -> str:
