a4584db0de69cc8ace73bef812961c9554f36748
Das Franck Hochstaetter <dasfranck@gmx.com>
2022-08-11 22:50:29 +0200

    #1 - Add TileLayer Enum with NORMAL, BIKE and SATELLITE values

---
+++ classes/TileLayer.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class TileLayer(Enum):
+    NORMAL = (
+        "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+        "OpenStreetMap"
+    )
+    CYCLE = (
+        "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
+        "Thunderforest and OpenStreetMap contributors"
+    )
+    SATELLITE = (
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        "Esri, Maxar, Earthstar Geographics, and the GIS User Community"
+    )
