d42a00d556bf3c830ef1d14c2280ee66f0357d03
Das Franck Hochstaetter <dasfranck@gmx.com>
2022-08-12 10:21:44 +0200

    #8 - Set default value for host arg to 127.0.0.1 (OpenTracksViewer.py)

--- OpenTracksViewer.py
+++ OpenTracksViewer.py
@@ -29,7 +29,7 @@ def load_gpxs(gpxs_path: str) -> dict[str, Track]:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("gpxs_path")
-    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=5000)
     parser.add_argument("--config", type=str, default="config.UserConfig", help="Config's object path")
     args = parser.parse_args()
