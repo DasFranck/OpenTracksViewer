d0be065ea13dc55154bc60fc3cde1b5eef32bdc4
DasFranck <dasfranck@gmx.com>
2022-08-24 01:20:21 +0200

    #12 - Add Outdoors Map

--- classes/tile_layer.py
+++ classes/tile_layer.py
@@ -4,11 +4,16 @@ from enum import Enum
 class TileLayer(Enum):
     NORMAL = (
         "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-        "OpenStreetMap"
+        "OpenStreetMap",
     )
     CYCLE = (
         "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
         "Thunderforest and OpenStreetMap contributors",
+    )
+    OUTDOORS = (
+        "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png",
+        "Thunderforest and OpenStreetMap contributors",
+
     )
     SATELLITE = (
         "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
