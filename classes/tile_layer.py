f4aa4d80eebcb1f9e58cb1e5fb92bc0b0d53f9bf
Franck Hochstaetter <dasfranck@gmx.com>
2023-07-27 15:40:41 +0200

    Change TileLayer from enum to dedicated dataclass and adapt codebase to this change

--- classes/tile_layer.py
+++ classes/tile_layer.py
@@ -1,29 +1,11 @@
 """Includes Tile Layer Enum."""
 
-from enum import Enum
+from dataclasses import dataclass
 
-
-class TileLayer(Enum):
+@dataclass
+class TileLayer:
     """
-    Enum value is a tuple of strings.
-    [0]: Generic tile format string
-    [1]: Attribution
-    [2]: API Key or None
     """
-
-    NORMAL = (
-        "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-        "OpenStreetMap",
-    )
-    CYCLE = (
-        "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
-        "Thunderforest and OpenStreetMap contributors",
-    )
-    OUTDOORS = (
-        "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png",
-        "Thunderforest and OpenStreetMap contributors",
-    )
-    SATELLITE = (
-        "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-        "Esri, Maxar, Earthstar Geographics, and the GIS User Community",
-    )
+    generic_tile_format: str
+    attribution: str
+    api_key: str | None = None
--- config.py
+++ config.py
@@ -22,6 +22,23 @@ class UserConfig:
     TIMEZONE = pytz.timezone("Europe/Paris")
 
     # Default Tile Layer to be used by Leaflet Maps
-    DEFAULT_TILE_LAYER = TileLayer.CYCLE
+    DEFAULT_TILE_LAYER = "Cycle"
 
-    DEFAULT_LOGGING_LEVEL = logging.INFO
+    TILE_LAYERS = {
+        "Normal": TileLayer(
+            "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
+            "OpenStreetMap",
+        ),
+        "Cycle": TileLayer(
+            "https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
+            "Thunderforest and OpenStreetMap contributors",
+        ),
+        "Outdoors": TileLayer(
+            "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png",
+            "Thunderforest and OpenStreetMap contributors",
+        ),
+        "Sattelite": TileLayer(
+            "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+            "Esri, Maxar, Earthstar Geographics, and the GIS User Community",
+        ),
+    }
